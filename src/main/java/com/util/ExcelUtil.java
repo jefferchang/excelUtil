ab63a348c62a3189acc8da2bf315b6fc59431906
常仪彪 <changyibiao126@163.com>
2016-07-20 10:57:51 +0800

    汽车之家生成sql

--- src/main/java/com/util/ExcelUtil.java
+++ src/main/java/com/util/ExcelUtil.java
@@ -4,9 +4,8 @@ package com.util;
  * Created by cyb on 2016/6/16.
  */
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.*;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -17,7 +16,7 @@ public class ExcelUtil {
 
     public static void main(String[] args) {
         File file = new File("/");
-        String path2007 = file.getAbsolutePath() + "excel\\src\\main\\resources\\test.xlsx";
+        String path2007 = file.getAbsolutePath() + "tools\\excelUtil\\src\\main\\resources\\car2.xlsx";
         parseExcel(path2007);
     }
     /**
@@ -38,7 +37,7 @@ public class ExcelUtil {
             if (workBook != null) {
                 int numberSheet = workBook.getNumberOfSheets();
                 if (numberSheet > 0) {
-                    sheet = workBook.getSheetAt(0);//获取第一个工作簿(Sheet)的内容
+                    sheet = workBook.getSheetAt(1);//获取第一个工作簿(Sheet)的内容
                     getExcelContent(sheet);
                 } else {
                     System.out.println("目标表格工作簿(Sheet)数目为0！");
@@ -58,15 +57,123 @@ public class ExcelUtil {
         int rowCount = sheet.getPhysicalNumberOfRows();//总行数
         int k = 0;
         if (rowCount > 1) {
-            for (int i = 0; i < rowCount; i++) {//遍历行，略过标题行，从第二行开始
+            String sql = "";
+            for (int i = 1; i < rowCount; i++) {//遍历行，略过标题行，从第二行开始
                 Row row = sheet.getRow(i);
-                for (int j = 0; j < 1; j++) {
-                    Cell cell = row.getCell(j);
-                    String bd = cell.getStringCellValue();
-                    System.out.println("insert into t_cj_promo_code values(" + ++k + ",'" + bd + "',0,sysdate,8); ");
+
+                String seriesid = null;
+                String c1 = null;
+                String c2 = null;
+                String c3 = null;
+                String c4 = null;
+                String c5 = null;
+                String c6 = null;
+                String c7 = null;
+                String c8 = null;
+                String c9 = null;
+                String c10 = null;
+                Cell cell = row.getCell(0);
+                if(cell != null){
+                      seriesid = cell.getStringCellValue();
                 }
+                Cell cell1 = row.getCell(1);
+                if(cell1 != null ){
+                     c1 = cell1.getStringCellValue();
+                }
+
+                Cell cell2 = row.getCell(2);
+
+                if(cell2 != null ){
+                     c2 = cell2.getStringCellValue();
+                }
+
+                Cell cell3 = row.getCell(3);
+                if(cell3 != null ){
+                      c3 = cell3.getStringCellValue();
+                }
+
+                Cell cell4 = row.getCell(4);
+                if(cell4 != null ){
+                      c4 = cell4.getStringCellValue();
+                }
+
+                Cell cell5 = row.getCell(5);
+                if(cell5 != null ){
+                      c5 = cell5.getStringCellValue();
+                }
+
+                Cell cell6 = row.getCell(6);
+                if(cell6 != null ){
+                     c6 = cell6.getStringCellValue();
+                }
+
+                Cell cell7 = row.getCell(7);
+                if(cell7 != null ){
+                      c7 = cell7.getStringCellValue();
+                }
+
+                Cell cell8 = row.getCell(8);
+                if(cell8 != null ){
+                     c8 = cell8.getStringCellValue();
+                }
+
+                Cell cell9 = row.getCell(9);
+                if(cell9 != null ){
+                    c9 = cell9.getStringCellValue();
+                }
+                Cell cell10 = row.getCell(10);
+                if(cell10 != null ){
+                      c10 = cell10.getStringCellValue();
+                }
+
+               /* System.out.println("insert into compete_series(id,seriesid,c1,c2,c3,c4,c5,c6,c7,c8,c9,c10) " +
+                        "values("+i+","+seriesid+","+c1+","+c2+","+c3+","+c4+","+c5+","+c6+","+c7+","+c8+","+c9+","+c10+"); ");
+*/
+                  sql += "insert into compete_spec(id,specid,s1,s2,s3,s4,s5,s6,s7,s8,s9,s10)values("+i+","+seriesid+","+c1+","+c2+","+c3+","+c4+","+c5+","+c6+","+c7+","+c8+","+c9+","+c10+"); \n ";
             }
+            //System.out.println(sql);
+            method1(sql);
+
         }
         return "";
+    }
+
+
+    public static void  method1(String content) {
+        FileWriter fw = null;
+        try {
+            File f=new File("E:\\sql.sql");
+            fw = new FileWriter(f, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        PrintWriter pw = new PrintWriter(fw);
+        pw.println(content);
+        pw.flush();
+        try {
+            fw.flush();
+            pw.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void method2(String file, String conent) {
+        BufferedWriter out = null;
+        try {
+            out = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(file, true)));
+            out.write(conent+"\r\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
